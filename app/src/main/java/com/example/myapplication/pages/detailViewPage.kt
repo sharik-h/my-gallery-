e1f3151cfd0937b822eeb97a9311cf3b2dab119c
Mohammed sharikh <mohammedsharik0123@gmail.com>
2023-10-15 09:59:59 +0530

    redesigned the detailViewPage

--- app/src/main/java/com/example/myapplication/pages/detailViewPage.kt
+++ app/src/main/java/com/example/myapplication/pages/detailViewPage.kt
@@ -1,22 +1,40 @@
 package com.example.myapplication.pages
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.widget.Toast
+import android.widget.Toast.makeText
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
+import com.example.myapplication.R
 import com.example.myapplication.data.ViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -24,11 +42,18 @@ import com.example.myapplication.data.ViewModel
 fun detailViewPage(navController: NavController, viewModel: ViewModel) {
 
     val image= viewModel.imageViewing.value!!
+    val context = LocalContext.current
+    val clipboard = ContextCompat.getSystemService(context, ClipboardManager::class.java)
+    val clip = ClipData.newPlainText("uri", image.url)
 
     Scaffold(
         topBar = {
             TopAppBar(
-                title = {},
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.back_text),
+                        style = MaterialTheme.typography.titleLarge
+                    ) },
                 navigationIcon = { 
                     IconButton(onClick = { navController.navigateUp() }) {
                         Icon(imageVector = Icons.Default.ArrowBack, contentDescription = null)
@@ -41,11 +66,67 @@ fun detailViewPage(navController: NavController, viewModel: ViewModel) {
                 .fillMaxSize()
                 .padding(it)) {
             
-            Image(painter = rememberAsyncImagePainter(model = image.download_url), contentDescription = null)
-            Column(modifier = Modifier.fillMaxSize().padding(10.dp)) {
-                Text(text = "By ${image.author}")
-                Text(text = "height & width : ${image.height}x${image.width}")
-                Text(text = "source : ${image.url}")
+            Image(
+                painter = rememberAsyncImagePainter(model = image.download_url),
+                contentDescription = null
+            )
+            Column(modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp)) {
+
+                Row(Modifier.fillMaxWidth()) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.user_icon),
+                        contentDescription = null
+                    )
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Text(
+                        text = "Author: ${image.author}",
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                }
+                Spacer(modifier = Modifier.height(10.dp))
+
+                Row(Modifier.fillMaxWidth()) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.aspect_ratio),
+                        contentDescription = null
+                    )
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Text(
+                        text = "Dimensions: ${image.height}x${image.width}",
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                }
+                Spacer(modifier = Modifier.height(0.dp))
+
+                Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.link),
+                        contentDescription = null
+                    )
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Text(
+                        text = "source : ${image.url}",
+                        style = MaterialTheme.typography.titleMedium,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        modifier = Modifier.weight(1f)
+                    )
+                    Row(
+                        modifier = Modifier.align(Alignment.CenterVertically),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        IconButton(onClick = {
+                            clipboard!!.setPrimaryClip(clip)
+                            makeText(context, "Text copied to clipboard", Toast.LENGTH_LONG).show()
+                        }) {
+                            Icon(painter = painterResource(id = R.drawable.content_copy), contentDescription = null)
+                        }
+                    }
+                }
+
             }
         }
     }
